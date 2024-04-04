9fa2b43d463b28b8b5b7b19a4559a4e80176c919
Ethen Mitterhuber <ethenmitterhuber@gmail.com>
2024-04-04 09:49:05 -0600

    Initial commit of project

---
+++ LibraryManagementSystem/LibraryManagementSystem/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace LibraryManagementSystem
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
