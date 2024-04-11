a944e053c9b94a75adc41b7b42087df9880fbde4
brokoli <bocalist@gmail.com>
2024-04-11 11:18:39 -0600

    Added global variable for authorization

--- LibraryManagementSystem/LibraryManagementSystem/Entities/SystemEnv.cs
+++ LibraryManagementSystem/LibraryManagementSystem/Entities/SystemEnv.cs
@@ -8,7 +8,29 @@ namespace LibraryManagementSystem.Models
 {
     public static class SystemEnv
     {
+        private static bool isAuthorized = false;
+
         public static readonly int RentalDaysForStudent = 14;
         public static readonly int RentalDaysForInstructor = 30;
+        public static bool GetIsAuthorized 
+        {
+            get 
+            {
+                return isAuthorized;
+            }
+
+            set
+            {
+                isAuthorized = value;
+            }
+        }
+    }
+
+    public enum UserType
+    {
+        Student,
+        Instructor,
+        Administrator,
+        Librarian
     }
 }
