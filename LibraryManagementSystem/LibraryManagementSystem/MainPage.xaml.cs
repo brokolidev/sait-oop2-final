5fc24058c83dc0edc329e909ed9915f63717be46
brokoli <bocalist@gmail.com>
2024-04-11 10:39:46 -0600

    added login button and its method

--- LibraryManagementSystem/LibraryManagementSystem/MainPage.xaml.cs
+++ LibraryManagementSystem/LibraryManagementSystem/MainPage.xaml.cs
@@ -11,6 +11,23 @@
         {
             Shell.Current.GoToAsync(nameof(InventoryPage));
         }
+
+        // Login
+        private void LoginButton_Clicked(object sender, EventArgs e)
+        {
+            string username = UsernameEntry.Text;
+            string password = PasswordEntry.Text;
+
+            // hardcoded username and password, should be replaced with real account with db connection
+            if(username == "admin" && password == "admin")
+            {
+                System.Diagnostics.Debug.WriteLine("Login successful");
+            }
+            else
+            {
+                DisplayAlert("Error", "Invalid username or password", "OK");
+            }
+        }
     }
 
 }
