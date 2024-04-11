140a5ae7dde88b5c5babf91876234cb76ab3329b
narapark7561 <narapark7561@gmail.com>
2024-04-11 15:21:22 -0600

    working on listview for book list on Inventory Page

--- LibraryManagementSystem/LibraryManagementSystem/Entities/Book.cs
+++ LibraryManagementSystem/LibraryManagementSystem/Entities/Book.cs
@@ -15,9 +15,9 @@ namespace LibraryManagementSystem.Entities
         public required string Title { get; set; }
         public required string Author { get; set; }
         public string? Publisher { get; set; }
-        public required Category Category { get; set; }
-        public required DateTime DatePublished { get; set; }
-        public required DateTime DateRegistered { get; set; }
+        //public required Category Category { get; set; }
+        //public required DateTime DatePublished { get; set; }
+        //public required DateTime DateRegistered { get; set; }
         public DateTime? DateUpdated { get; set; }
         public int? Total { get; set; }
         public int? CheckedOut { get; set; }
