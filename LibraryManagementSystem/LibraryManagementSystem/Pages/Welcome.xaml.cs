ec339df1fbe73aec0e5ec6e0047fd82662a0cfc9
brokoli <bocalist@gmail.com>
2024-04-11 15:34:44 -0600

    menu permission set by logged in user types

--- LibraryManagementSystem/LibraryManagementSystem/Pages/Welcome.xaml.cs
+++ LibraryManagementSystem/LibraryManagementSystem/Pages/Welcome.xaml.cs
@@ -1,4 +1,5 @@
 using LibraryManagementSystem.Config;
+using LibraryManagementSystem.Entities;
 
 namespace LibraryManagementSystem.Pages;
 
@@ -18,6 +19,22 @@ public partial class Welcome : ContentPage
         WelcomMessageLabel.Text = $"Welcome " +
             $"{SystemEnv.LoggedInUser.FirstName}," +
             $" You are logged in as {SystemEnv.LoggedInUser.UserType}";
+
+        // set buttons by user types
+        RentalButton.IsVisible = 
+            SystemEnv.LoggedInUser.UserType == User.UserTypes.Student || 
+            SystemEnv.LoggedInUser.UserType == User.UserTypes.Instructor;
+
+        CustomerButton.IsVisible = 
+            SystemEnv.LoggedInUser.UserType == User.UserTypes.Librarian|| 
+            SystemEnv.LoggedInUser.UserType == User.UserTypes.Administrator;
+
+        InventoryButton.IsVisible =
+            SystemEnv.LoggedInUser.UserType == User.UserTypes.Librarian ||
+            SystemEnv.LoggedInUser.UserType == User.UserTypes.Administrator;
+
+        SystemButton.IsVisible = 
+            SystemEnv.LoggedInUser.UserType == User.UserTypes.Administrator;
     }
 
     private void InventoryButton_Clicked(object sender, EventArgs e)
