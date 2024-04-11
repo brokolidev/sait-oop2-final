171fe2a9b286ce0898e7d41b301be2cd9a0f6275
brokoli <bocalist@gmail.com>
2024-04-10 22:26:51 -0600

    Added Rental Class

---
+++ LibraryManagementSystem/LibraryManagementSystem/Models/Rental.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Models
+{
+    public  class Rental
+    {
+        public int Id { get; set; }
+        public int BookId { get; set; }
+        public int UserId { get; set; }
+        public DateTime RentedAt { get; set; }
+        public DateTime ReturnedAt { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        public Rental() { }
+    }
+}
