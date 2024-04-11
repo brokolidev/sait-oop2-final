a47a223a27b5cc5830ddb74278a61d0a0001d45f
brokoli <bocalist@gmail.com>
2024-04-10 22:29:53 -0600

    Added SystemEnv Class

---
+++ LibraryManagementSystem/LibraryManagementSystem/Models/SystemEnv.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Models
+{
+    public class SystemEnv
+    {
+        public int RentalDaysForStudent { get; set; }
+        public int RentalDaysForInstructor { get; set; }
+
+
+        public SystemEnv()
+        {
+            // Default values
+            // This can be changed by the administrator
+            // need to be stored in the database
+            RentalDaysForStudent = 14;
+            RentalDaysForInstructor = 30;
+        }
+    }
+}
