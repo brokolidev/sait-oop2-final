6b453f1ee8fb4a0f2a11895b8515282a857e88bf
brokoli <bocalist@gmail.com>
2024-04-12 10:47:36 -0600

    added placeholder for database part

--- LibraryManagementSystem/LibraryManagementSystem/Pages/CustomerPage.xaml.cs
+++ LibraryManagementSystem/LibraryManagementSystem/Pages/CustomerPage.xaml.cs
@@ -1,15 +1,21 @@
 using LibraryManagementSystem.Config;
 using LibraryManagementSystem.Entities;
+using LibraryManagementSystem.Persistence.Controllers;
 using System.Diagnostics;
 
 namespace LibraryManagementSystem.Pages;
 
 public partial class CustomerPage : ContentPage
 {
+
+    //List<User> users;
+    //UserController userController;
+
 	public CustomerPage()
 	{
 		InitializeComponent();
 	}
+
     protected override void OnAppearing()
     {
         base.OnAppearing();
@@ -29,29 +35,21 @@ public partial class CustomerPage : ContentPage
 
         SystemButton.IsVisible =
             SystemEnv.LoggedInUser is Administrator;
+
+        // @TODO: Get all users from the database
+        //userController = new UserController();
+        //users = userController.GetAllUsers();
     }
 
+    // Main navigation buttons
     private void InventoryButton_Clicked(object sender, EventArgs e)
     {
         Shell.Current.GoToAsync(nameof(InventoryPage));
     }
 
-    private void LogoutButton_Clicked(object sender, EventArgs e)
-    {
-        SystemEnv.LoggedInUser = null;
-        SystemEnv.IsAuthorized = false;
-
-        Shell.Current.GoToAsync($"//{nameof(MainPage)}");
-    }
-
     private void HomeButton_Clicked(object sender, EventArgs e)
     {
         Shell.Current.Navigation.PopToRootAsync();
-    }
-
-    private void AddCustButton_Clicked(object sender, EventArgs e)
-    {
-        Shell.Current.GoToAsync(nameof(AddCustomerPage));
     }
 
     private void CustomerButton_Clicked(object sender, EventArgs e)
@@ -62,6 +60,13 @@ public partial class CustomerPage : ContentPage
     private void SystemButton_Clicked(object sender, EventArgs e)
     {
         Shell.Current.GoToAsync(nameof(SystemPage));
+    }
+
+
+    // Customer manager buttons
+    private void AddCustButton_Clicked(object sender, EventArgs e)
+    {
+        Shell.Current.GoToAsync(nameof(AddCustomerPage));
     }
 
 }
