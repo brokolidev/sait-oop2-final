f6866b18764cda7a7f2976989d994bb2dd970d21
narapark7561 <narapark7561@gmail.com>
2024-04-11 18:26:02 -0600

    Completed Add customer page

--- LibraryManagementSystem/LibraryManagementSystem/Pages/AddCustomerPage.xaml.cs
+++ LibraryManagementSystem/LibraryManagementSystem/Pages/AddCustomerPage.xaml.cs
@@ -1,3 +1,7 @@
+using LibraryManagementSystem.Config;
+using LibraryManagementSystem.Entities;
+using System.Diagnostics;
+
 namespace LibraryManagementSystem.Pages;
 
 public partial class AddCustomerPage : ContentPage
@@ -6,4 +10,30 @@ public partial class AddCustomerPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+        // set buttons by user types
+        RentalButton.IsVisible =
+            SystemEnv.LoggedInUser.UserType == User.UserTypes.Student ||
+            SystemEnv.LoggedInUser.UserType == User.UserTypes.Instructor;
+
+        CustomerButton.IsVisible =
+            SystemEnv.LoggedInUser.UserType == User.UserTypes.Librarian ||
+            SystemEnv.LoggedInUser.UserType == User.UserTypes.Administrator;
+
+        InventoryButton.IsVisible =
+            SystemEnv.LoggedInUser.UserType == User.UserTypes.Librarian ||
+            SystemEnv.LoggedInUser.UserType == User.UserTypes.Administrator;
+
+        SystemButton.IsVisible =
+            SystemEnv.LoggedInUser.UserType == User.UserTypes.Administrator;
+    }
+
+    private void HomeButton_Clicked(object sender, EventArgs e)
+    {
+        Shell.Current.Navigation.PopToRootAsync();
+    }
 }
