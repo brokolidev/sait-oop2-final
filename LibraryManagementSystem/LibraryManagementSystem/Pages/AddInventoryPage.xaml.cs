def1cb0b6bcf1d29e0342ef810254e899294566b
brokoli <bocalist@gmail.com>
2024-04-13 11:21:24 -0600

    Implemented save new book method

--- LibraryManagementSystem/LibraryManagementSystem/Pages/AddInventoryPage.xaml.cs
+++ LibraryManagementSystem/LibraryManagementSystem/Pages/AddInventoryPage.xaml.cs
@@ -8,12 +8,14 @@ namespace LibraryManagementSystem.Pages;
 public partial class AddInventoryPage : ContentPage
 {
     List<Category> categories;
+    Category selectedCategory;
 
 	public AddInventoryPage()
 	{
 		InitializeComponent();
 	}
 
+    // set categories for the picker
     private void SetCategories()
     {
         // set categories
@@ -23,11 +25,13 @@ public partial class AddInventoryPage : ContentPage
         categoryPicker.ItemsSource = categories;
         categoryPicker.ItemDisplayBinding = new Binding("Name");
         categoryPicker.SelectedIndex = 0;
+        selectedCategory = categoryPicker.SelectedItem as Category;
 
         // bind the event handler
         categoryPicker.SelectedIndexChanged += OnCategoryIndexChanged;
     }
 
+    // when the page is shown
     protected override void OnAppearing()
     {
         base.OnAppearing();
@@ -55,16 +59,57 @@ public partial class AddInventoryPage : ContentPage
     private void OnCategoryIndexChanged(object sender, EventArgs e)
     {
         var picker = (Picker)sender;
-        int selectedIndex = picker.SelectedIndex;
-
-        Category selectedCategory = categories[selectedIndex];
-        Debug.WriteLine("Selected Category: " + selectedCategory.Name);
+        selectedCategory = picker.SelectedItem as Category;
     }
 
+    // add book to inventory
     private void SaveButton_Clicked(object sender, EventArgs e)
     {
-        // @TODO: save new book to database
-        // after successfully saved the book, navigate back to InventoryPage
+        // validate category
+        if(selectedCategory == null)
+        {
+            DisplayAlert("Error", "Please select a category", "OK");
+            return;
+        }
+
+        // validate other text fields
+        if (string.IsNullOrEmpty(isbnEntry.Text) || string.IsNullOrEmpty(titleEntry.Text) || string.IsNullOrEmpty(authorEntry.Text) || string.IsNullOrEmpty(publisherEntry.Text) || string.IsNullOrEmpty(publishDateEntry.Text))
+        {
+            DisplayAlert("Error", "Please fill in all fields", "OK");
+            return;
+        }
+
+        // validate published date input
+        DateOnly publishedDate;
+        bool publishedDateParsed = DateOnly.TryParse(publishDateEntry.Text, out publishedDate);
+
+        if (!publishedDateParsed)
+        {
+            DisplayAlert("Error", "Please enter a valid date", "OK");
+            return;
+        }
+
+        // create book
+        BookController bookController = new BookController();
+
+        Book newBook = new Book();
+        newBook.ISBN = isbnEntry.Text;
+        newBook.Title = titleEntry.Text;
+        newBook.Author = authorEntry.Text;
+        newBook.Publisher = publisherEntry.Text;
+        newBook.DatePublished = publishedDate;
+        
+        // set category for the new book 
+        newBook.Category = selectedCategory;
+
+        // update dates
+        newBook.DateRegistered = DateOnly.FromDateTime(DateTime.Now);
+        newBook.DateUpdated = DateOnly.FromDateTime(DateTime.Now);
+        
+        bookController.CreateBook(newBook);
+
+        // go back to inventory page after updating book
+        Shell.Current.GoToAsync(nameof(InventoryPage));
     }
 
     // Go back to listing page
