7405d2ebab1d3eb625185cf72fbfd7e47504215d
brokoli <bocalist@gmail.com>
2024-04-18 20:28:29 -0600

    Added Rental return function in rental history for administrators

--- LibraryManagementSystem/LibraryManagementSystem/Pages/RentalList.xaml.cs
+++ LibraryManagementSystem/LibraryManagementSystem/Pages/RentalList.xaml.cs
@@ -1,6 +1,8 @@
 using LibraryManagementSystem.Config;
 using LibraryManagementSystem.Entities;
 using LibraryManagementSystem.Persistence.Controllers;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 
 namespace LibraryManagementSystem.Pages;
 
@@ -20,7 +22,8 @@ public partial class RentalList : ContentPage
     private void SetRentalList()
     {
         rentals = rentalController.GetAllRentals();
-        RentalListView.ItemsSource = rentals;
+        var rentalCollection = new ObservableCollection<Rental>(rentals);
+        RentalListView.ItemsSource = rentalCollection;
         //RentalListView.ItemSelected += OnCustomerSelected;
     }
 
@@ -28,6 +31,33 @@ public partial class RentalList : ContentPage
     {
         base.OnAppearing();
 
+        SetRentalList();
+    }
+
+    // Return Rental
+    private async void ReturnButton_Clicked(object sender, EventArgs e)
+    {
+        // display alert to confirm deletion
+        bool answer = await DisplayAlert("Delete", "Are you sure you want to return this book?", "Yes", "No");
+
+        if (!answer)
+        {
+            return;
+        }
+
+        var button = sender as Button;
+        int rentalId = (int)button.CommandParameter;
+
+        // return rental
+        Rental selectedRental = rentalController.GetRental(rentalId);
+
+        selectedRental.DateReturned = DateOnly.FromDateTime(DateTime.Now);
+        rentalController.UpdateRental(selectedRental);
+
+        // display success message
+        await DisplayAlert("Success", "Book returned successfully", "OK");
+
+        // refresh list
         SetRentalList();
     }
 }
